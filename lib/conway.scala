b519c6de9472ea1dd4599de9c105832fa8586b2e
douglascodes <douglasking215@gmail.com>
2013-01-26 03:43:43 -0500

    Initilized Petry dish

--- lib/conway.scala
+++ lib/conway.scala
@@ -6,8 +6,8 @@ object Dish {
 //     will seek to utilize the best techniques in Scala.
 //     """
   val measure: Double = 8.5
-  val limit: Double = (measure * measure).toInt 
-  val max_cells: Int = (limit * limit).toInt
+  val limit: Int = (measure * measure).toInt 
+  val max_cells: Int = (limit * limit)
   val vigor: Double = (30.0 / 100.0)
   val start_count: Int = (max_cells * vigor).toInt
 
@@ -17,27 +17,16 @@ object Dish {
   var next_gen: Set[Tuple2[Int, Int]] = Set()
   var prev_gen: Set[Tuple2[Int, Int]] = Set()
   var cells: Set[Tuple2[Int, Int]] = Set()  
-  spawn
+  spawn()
 
-  def spawn { 
-  // var x = rand(0, limit-1)
-  // var y = rand(0, limit-1)
-  
-  // for z in range(count):
-  //     while ((x, y)) in a_set
-  //         x = rand(0, limit-1)
-  //         y = rand(0, limit-1)
-  //     a_set.add((x, y))
+  def spawn() = {
+    // One time call to randomly populate the Dish.cells value with random Tuple2[Int,Int]'s
+    while ( start_count != cells.size ) {
+      val x: Int = ((scala.math.random*(limit + 1)).toInt)
+      val y: Int = ((scala.math.random*(limit + 1)).toInt)
+      cells = cells ++ Set((x, y)) 
+    }
   }
-}
 
-// def printMeRands {
-// var x: Int = ((scala.math.random*(101)).toInt)
-// var y: Int = x
-// while(y != 100 ) {
-// x = ((scala.math.random*(101)).toInt)
-// print(x)
-// print(" ")
-// y = x
-// }
-// }
+
+}
